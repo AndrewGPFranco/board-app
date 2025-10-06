b2cc980f4c9bcfdea28165f38582b07634fe74d6
AndrewGPFranco <andrewgomes1328@gmail.com>
2025-10-05 21:06:11 -0300

    inicial commit

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+import {View, Text, StyleSheet} from 'react-native';
+
+export default function Home() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Olá APP</Text>
+            <Text>Funcionou!</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+    },
+});
